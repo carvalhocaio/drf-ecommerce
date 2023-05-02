37d522f55730c1b4be9d3a67dac09898214b4714
carvalhocaio <caiocarvalho.py@gmail.com>
2023-05-02 19:10:09 -0400

    implementing drf router

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from core.product import views
+
+router = DefaultRouter()
+router.register(r"category", views.CategoryViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
 ]
