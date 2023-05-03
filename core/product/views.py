720dabac8135445a9184966a9f64979919a2430c
carvalhocaio <caiocarvalho.py@gmail.com>
2023-05-02 21:57:39 -0400

    implementing brand

--- core/product/views.py
+++ core/product/views.py
@@ -2,13 +2,13 @@ from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from rest_framework.response import Response
 
-from .models import Category
-from .serializers import CategorySerializer
+from .models import Brand, Category, Product
+from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
 
 
 class CategoryViewSet(viewsets.ViewSet):
     """
-    A simple Viewset for viewing categories
+    A simple Viewset for viewing all categories
     """
 
     queryset = Category.objects.all()
@@ -16,4 +16,17 @@ class CategoryViewSet(viewsets.ViewSet):
     @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
+        return Response(serializer.data)
+
+
+class BrandViewSet(viewsets.ViewSet):
+    """
+    A simple Viewset for viewing all brands
+    """
+
+    queryset = Brand.objects.all()
+
+    @extend_schema(responses=BrandSerializer)
+    def list(self, request):
+        serializer = BrandSerializer(self.queryset, many=True)
         return Response(serializer.data)
