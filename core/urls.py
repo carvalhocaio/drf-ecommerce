1391679f1726ea8c8ce06013cbeec27512b65ae9
carvalhocaio <caiocarvalho.py@gmail.com>
2023-05-02 22:12:44 -0400

    implementing product

--- core/urls.py
+++ core/urls.py
@@ -8,6 +8,7 @@ from core.product import views
 router = DefaultRouter()
 router.register(r"category", views.CategoryViewSet)
 router.register(r"brand", views.BrandViewSet)
+router.register(r"products", views.ProductViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
